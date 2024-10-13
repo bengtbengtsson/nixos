1b0e7fd73c006000eb6346554eac08516c99e92e
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2024-10-13 19:28:01 +0200

    add Brave browser

--- configuration.nix
+++ configuration.nix
@@ -5,129 +5,139 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+	imports =
+		[ # Include the results of the hardware scan.
+		./hardware-configuration.nix
+		];
 
-  # Define LUKS encryption for the root partition
-  boot.initrd.luks.devices.cryptroot = {
-    device = lib.mkForce "/dev/disk/by-partlabel/cryptic";
-    preLVM = true;
-  }; 
+# Define LUKS encryption for the root partition
+	boot.initrd.luks.devices.cryptroot = {
+		device = lib.mkForce "/dev/disk/by-partlabel/cryptic";
+		preLVM = true;
+	}; 
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+# Use the systemd-boot EFI boot loader.
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  # Mount the root filesystem
-  fileSystems."/".device = "/dev/disk/by-label/nixos";
+# Mount the root filesystem
+	fileSystems."/".device = "/dev/disk/by-label/nixos";
 
-  # Specify the boot partition
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
+# Specify the boot partition
+	fileSystems."/boot" = {
+		device = "/dev/disk/by-label/boot";
+		fsType = "vfat";
+	};
+
+	networking.hostName = "nixos"; # Define your hostname.
+		networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+# Set your time zone.
+		time.timeZone = "Europe/Stockholm";
+
+# Select internationalisation properties.
+	i18n.defaultLocale = "en_US.UTF-8";
+	console = {
+		font = "Lat2-Terminus16";
+		keyMap = "sv-latin1";
+#   useXkbConfig = true; # use xkb.options in tty.
+	};
+
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
   };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
-  time.timeZone = "Europe/Stockholm";
+	services.xserver.enable = true;
+	services.xserver.xkb.layout = "se";
+	services.libinput.enable = true;
+	services.libinput.touchpad.naturalScrolling = true;
+	services.xserver.displayManager.lightdm.enable = false;
+	services.xserver.displayManager.startx.enable = true;
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "sv-latin1";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  };
+# Enable CUPS to print documents.
+# services.printing.enable = true;
 
-  services.xserver.enable = true;
-  services.xserver.xkb.layout = "se";
-  services.libinput.enable = true;
-  services.libinput.touchpad.naturalScrolling = true;
-  services.xserver.displayManager.lightdm.enable = false;
-  services.xserver.displayManager.startx.enable = true;
+# Enable sound.
+# hardware.pulseaudio.enable = true;
+# OR
+# services.pipewire = {
+#   enable = true;
+#   pulse.enable = true;
+# };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+# Define a user account. Don't forget to set a password with ‘passwd’.
+	users.users.ben = {
+		isNormalUser = true;
+		home = "/home/ben";
+		shell = pkgs.bash;
+		extraGroups = [ "wheel" "video" ]; # Enable ‘sudo’ for the user.
+	};
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.ben = {
-    isNormalUser = true;
-    home = "/home/ben";
-    shell = pkgs.bash;
-    extraGroups = [ "wheel" "video" ]; # Enable ‘sudo’ for the user.
-  };
+# Enable sudo for users in the 'wheel' group
+	security.sudo = {
+		enable = true;
+		wheelNeedsPassword = true;  # Prompt for password when using sudo
+	};
 
-  # Enable sudo for users in the 'wheel' group
-  security.sudo = {
-    enable = true;
-    wheelNeedsPassword = true;  # Prompt for password when using sudo
-  };
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+	environment.systemPackages = with pkgs; [
+		vim
+			wget
+			git
+			dwm
+			st
+			dmenu
+			chromium
+			parted
+brave
+	];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    git
-    dwm
-    st
-    dmenu
-    chromium
-    parted
-  ];
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+# List services that you want to enable:
 
-  # List services that you want to enable:
+# Enable the OpenSSH daemon.
+	services.openssh.enable = true;
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+	networking.firewall.enable = false;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+# Copy the NixOS configuration file and link it from the resulting system
+# (/run/current-system/configuration.nix). This is useful in case you
+# accidentally delete configuration.nix.
+# system.copySystemConfiguration = true;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05"; # Did you read the comment?
+# This option defines the first version of NixOS you have installed on this particular machine,
+# and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+#
+# Most users should NEVER change this value after the initial install, for any reason,
+# even if you've upgraded your system to a new NixOS release.
+#
+# This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+# so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+# to actually do that.
+#
+# This value being lower than the current NixOS release does NOT mean your system is
+# out of date, out of support, or vulnerable.
+#
+# Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+# and migrated your data accordingly.
+#
+# For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+	system.stateVersion = "24.05"; # Did you read the comment?
 }
